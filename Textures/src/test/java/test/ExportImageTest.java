38ba044343e098f7b0d9019cad71a3ffbdb42b3d
andbeder <andbeder@gmail.com>
2025-06-08 16:00:18 -0400

    Fix export save and add unit test

---
+++ Textures/src/test/java/test/ExportImageTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import com.beder.texture.TextureGenius;
+import com.beder.texture.TextureGUI;
+import com.beder.texture.ImagePair;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExportImageTest {
+    @Test
+    public void exportWritesFiles() throws Exception {
+        TextureGenius genius = new TextureGenius(4);
+        TextureGUI gui = new TextureGUI(genius);
+        ImagePair pair = genius.getCurrentImage();
+        pair.left.setRGB(0, 0, 0xFF00FF00);
+        pair.right.setRGB(0, 0, 0xFFFF0000);
+
+        File base = File.createTempFile("tex", ".png");
+        base.deleteOnExit();
+        gui.exportCurrentImage(base);
+
+        String name = base.getName().replaceFirst("(\\.[^.]+)?$", "");
+        File left = new File(base.getParentFile(), name + "_left.png");
+        File right = new File(base.getParentFile(), name + "_right.png");
+
+        assertTrue(left.exists());
+        assertTrue(right.exists());
+
+        BufferedImage limg = ImageIO.read(left);
+        BufferedImage rimg = ImageIO.read(right);
+        assertEquals(0xFF00FF00, limg.getRGB(0, 0));
+        assertEquals(0xFFFF0000, rimg.getRGB(0, 0));
+
+        left.delete();
+        right.delete();
+    }
+}
